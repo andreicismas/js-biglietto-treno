f6a7ac4329388fe5a611c3c9a0aea5bf7891213c
andreicismas <andrei_cismas@yahoo.it>
2021-04-08 18:43:28 +0200

    fin project

--- script.js
+++ script.js
@@ -6,21 +6,16 @@ console.log ("vuole percorrere" ," ", + numberKm ,"km");
 var yourAge = parseInt(prompt("la tua eta?"))
 console.log ("eta" ," ", + yourAge ,"");
 
-
 // price km kids
-var priceKm = 0.21 ;
+var priceKm = (numberKm * 0.21) ;
 var kidsPrice =  (priceKm * 20 / 100);
-var totalKidsPrice = kidsPrice * numberKm;
 
 console.log ("prezzo per Km",priceKm);
 console.log ("prezzo per minorenni sconto del 20% al km =",kidsPrice , "€");
-console.log ("prezzo totale km per minorenni ",totalKidsPrice, "€");
-
-
+console.log ("prezzo totale km per minorenni ",kidsPrice, "€");
 
 // var totalOver_65 = over_65 + priceKm;
 var over65 =  (priceKm * 40 / 100);
-var totalOver_65 =( over65 * numberKm);
 
 console.log ("prezzo per Km",priceKm);
 console.log ("prezzo per over 65 sconto del 40% al km =",over65 );
@@ -32,8 +27,11 @@ if(Number.isNaN(numberKm) || Number.isNaN(yourAge)){
     document.getElementById("real_cost").innerHTML= "I valori inseriti non corretti";
 }else{
     if(yourAge < 18){
-        document.getElementById("real_cost").innerHTML="il tuo prezzo e " + totalKidsPrice.toFixed(2) + " "+ "€ con 20% di sconto"
-    }else if (yourAge > 18){
-        document.getElementById("real_cost").innerHTML="il tuo prezzo e " + totalOver_65.toFixed(2) + " "+ "€ con 40% di sconto"
+        document.getElementById("real_cost").innerHTML="il tuo prezzo e " + kidsPrice.toFixed(2) + " "+ "€ con 20% di sconto"
+    }else if (yourAge > 65){
+        document.getElementById("real_cost").innerHTML="il tuo prezzo e " + over65.toFixed(2) + " "+ "€ con 40% di sconto"
+    }
+    else{
+        document.getElementById("real_cost").innerHTML="il tuo prezzo e " + priceKm.toFixed(2) + " "+ "€ con 0% di sconto"
     }
 }
